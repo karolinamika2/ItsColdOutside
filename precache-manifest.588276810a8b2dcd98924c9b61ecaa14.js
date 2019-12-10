978f9216490df3e66f5a2c8e3d187202651bf543
Karolina <karolinamika2@gmail.com>
2019-12-10 22:53:40 +0100

    Updates

---
+++ precache-manifest.588276810a8b2dcd98924c9b61ecaa14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac049c2b4742a597671a4cfa9b2e8851",
+    "url": "/ItsColdOutside/index.html"
+  },
+  {
+    "revision": "0ef51520cfb0154f875d",
+    "url": "/ItsColdOutside/static/css/main.388f357c.chunk.css"
+  },
+  {
+    "revision": "d5ed803b6c871c7b79ef",
+    "url": "/ItsColdOutside/static/js/2.8c293e03.chunk.js"
+  },
+  {
+    "revision": "0ef51520cfb0154f875d",
+    "url": "/ItsColdOutside/static/js/main.f19d80e9.chunk.js"
+  },
+  {
+    "revision": "28e7ff61d279b98c686a",
+    "url": "/ItsColdOutside/static/js/runtime-main.bd96ae5d.js"
+  }
+]);
